b4321acca3b64c9d953ebefda97347ed28f12c2e
oMegaPB <96842602+oMegaPB@users.noreply.github.com>
2022-08-06 23:42:16 +0300

    Delete Viola directory

--- Viola/Config/settings.py
+++
@@ -1,5 +0,0 @@
-token = "OTI0MzU3NTE3MzA2MzgwMzc4.YcdZBQ.VjgRCbVdzotX0I6-HGQiavu7ZcA"
-
-apikey = "6841122f-a0ac-4db9-88ec-52a5e0b36f13"
-
-token_beta = 'OTMxODczNjc1NDU0NTk1MTAy.GicLdn.GV0ArhWxJMbxlVp8F1902sB2L8llLG0oWqZlMI'
