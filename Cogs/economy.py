1eb449e19966ac61dd585daf22c09ee5327a2278
oMegaPB <96842602+oMegaPB@users.noreply.github.com>
2022-09-02 00:48:59 +0300

    Update economy.py

--- Cogs/economy.py
+++ Cogs/economy.py
@@ -8,12 +8,12 @@ class economy(commands.Cog, description='**Экономика.**'):
     def __init__(self, bot: Viola):
         self.bot = bot
     
-    @commands.command()
-    async def work(self, ctx: commands.Context):
-        now = datetime.datetime.now().timestamp()
-        res = await self.bot.bd.fetch({'guildid': ctx.guild.id}, category='workers')
-        if res.status:
-            return await ctx.channel.send
+#     @commands.command()
+#     async def work(self, ctx: commands.Context):
+#         now = datetime.datetime.now().timestamp()
+#         res = await self.bot.bd.fetch({'guildid': ctx.guild.id}, category='workers')
+#         if res.status:
+#             return await ctx.channel.send
 # -----------------------------------------------------------------------------------------------------------
 async def setup(bot: Viola):
     await bot.add_cog(economy(bot))
